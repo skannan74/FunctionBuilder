74884cde45ffbffd9a85193f7dfdb83c03eada88
kannan.s <gonehybrid@gmail.com>
2016-11-02 16:41:25 +0530

    added console.test app

---
+++ MobiasFunctionBuilder.ConsoleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobiasFunctionBuilder.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fn = Sample.BuildLambda();
+
+            Sample.mDictionary result = fn.Compile().DynamicInvoke() as Sample.mDictionary;
+        }
+    }
+}
